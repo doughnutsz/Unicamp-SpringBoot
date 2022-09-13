474203555b3dfcc2fc93e2d1a5909adc27a3b908
SeaSkysz <2448861628@qq.com>
2022-09-13 12:20:55 +0800

    Initial commit

---
+++ src/main/java/com/perfectmatch/unicampspringboot/UnicampSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.perfectmatch.unicampspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnicampSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnicampSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perfectmatch/unicampspringboot/UnicampSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfectmatch.unicampspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnicampSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
