4cc34b048fd195d2c583c77a1857de2272263ac4
SeaSkysz <2448861628@qq.com>
2022-09-13 16:45:22 +0800

    Hello, World!

--- src/main/java/com/perfectmatch/unicampspringboot/UnicampSpringBootApplication.java
+++ src/main/java/com/perfectmatch/unicampspringboot/UnicampSpringBootApplication.java
@@ -2,12 +2,13 @@ package com.perfectmatch.unicampspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class UnicampSpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UnicampSpringBootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UnicampSpringBootApplication.class, args);
+    }
 
 }
