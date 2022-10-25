cb2be61e6578e42484d7492ec679f6febd94731e
maze <782618517@qq.com>
2022-10-25 16:04:58 +0800

    favorite bug fix,test ok

--- src/main/java/com/perfectmatch/unicampspringboot/mapper/FavoriteMapper.java
+++ src/main/java/com/perfectmatch/unicampspringboot/mapper/FavoriteMapper.java
@@ -7,11 +7,11 @@ import java.util.List;
 
 @Mapper
 public interface FavoriteMapper {
-    FavoriteDao getFavorite(String used_id, String course_id);
+    FavoriteDao getFavorite(String user_id, String course_id);
 
     void insertFavorite(FavoriteDao dao);
 
-    void deleteFavorite(String used_id, String course_id);
+    void deleteFavorite(String user_id, String course_id);
 
     List<Long> selectFavoriteByUserId(String user_id);
 
